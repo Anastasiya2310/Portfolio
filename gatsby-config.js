7ef3ba60981ac0b20288280442f8c08723f51f26
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-06-24 16:58:30 +0200

    footer icons: styling; content

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,13 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /images\/icons/,
+        }
+      }
+    },
   ]
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import * as FooterStyle from './footer.module.css'
 import * as LayoutStyle from './../styles/layout.module.css'
 import * as TypographyStyle from './../styles/typography.module.css'
+import * as FooterStyle from './footer.module.css'
 import { useStaticQuery, graphql } from 'gatsby'
+import GithubIcon from '../images/icons/github.svg'
+import EmailIcon from '../images/icons/email.svg'
+import LinkedinIcon from '../images/icons/linkedin.svg'
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -25,17 +28,24 @@ const Footer = () => {
 
   return (
     <footer className={`${FooterStyle.footer} ${LayoutStyle.container}`}>
-      <ul className={`${TypographyStyle.listStyleNone} ${LayoutStyle.flex} ${LayoutStyle.fRow} ${LayoutStyle.gap10} ${LayoutStyle.fJustifyCenter}`}>
-        <li key={contact.email} className={`${FooterStyle.email} ${FooterStyle.iconFooter}`}>
-          <a href={contact.email} target="_blank" rel="noreferrer noopener">Email</a>
+      <ul className={`${FooterStyle.contactList} ${TypographyStyle.listStyleNone} ${LayoutStyle.flex} ${LayoutStyle.fRow} ${LayoutStyle.fJustifyCenter} ${LayoutStyle.fAlignItemsCenter}`}>
+        <li key={contact.email} className={`${LayoutStyle.flex} ${LayoutStyle.fJustifyCenter} ${LayoutStyle.fAlignItemsCenter}`}>
+          <a href={`mailto:${contact.email}`} target="_blank" rel="noreferrer noopener">
+            <EmailIcon width={38} height={38}/>
+          </a>
         </li>
-        <li key={contact.github} className={`${FooterStyle.github} ${FooterStyle.iconFooter}`}>
-          <a href={contact.github} target="_blank" rel="noreferrer noopener">Github</a>
+        <li key={contact.github} className={`${FooterStyle.iconFooter} ${LayoutStyle.flex} ${LayoutStyle.fJustifyCenter} ${LayoutStyle.fAlignItemsCenter}`}>
+          <a href={contact.github} target="_blank" rel="noreferrer noopener">
+            <GithubIcon width={30} height={30}/>
+          </a>
         </li>
-        <li key={contact.linkedin} className={`${FooterStyle.linkedin} ${FooterStyle.iconFooter}`}>
-          <a href={contact.linkedin} target="_blank" rel="noreferrer noopener">Linkedin</a>
+        <li key={contact.linkedin} className={`${FooterStyle.iconFooter} ${LayoutStyle.flex} ${LayoutStyle.fJustifyCenter} ${LayoutStyle.fAlignItemsCenter}`}>
+          <a href={contact.linkedin} target="_blank" rel="noreferrer noopener">
+            <LinkedinIcon width={38} height={38}/>
+          </a>
         </li>
       </ul>
+      <p className={`${FooterStyle.textFooter} ${TypographyStyle.textCenter}`}>Built with <a href="https://www.gatsbyjs.com/" className={`${FooterStyle.linksFooter}`} target="_blank" rel="noreferrer noopener"> Gatsby</a>, deployed with <a href="https://www.netlify.com/" className={`${FooterStyle.linksFooter}`} target="_blank" rel="noreferrer noopener"> Netlify</a></p>
     </footer>
   );
 };
