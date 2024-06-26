eba2f5525ecb7d49cc588be440e145348182d873
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-06-26 14:24:02 +0200

    fixing issues with commonJS & ES6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import "normalize.css";
 
-exports.onClientEntry = () => {
+export const onClientEntry = () => {
   if (typeof window !== "undefined") {
     const script = document.createElement("script");
     script.src = "https://platform-api.sharethis.com/js/sharethis.js#property=667bf64dd99dc7001ae46dc9&product=inline-share-buttons";
