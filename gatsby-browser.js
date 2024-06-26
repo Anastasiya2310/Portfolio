8bb378adb0edc3fa39b4299f0b30d2e7b9363dde
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-06-26 15:57:03 +0200

    remove sharing icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,1 @@
 import "normalize.css";
-
-export const onClientEntry = () => {
-  if (typeof window !== "undefined") {
-    const script = document.createElement("script");
-    script.src = "https://platform-api.sharethis.com/js/sharethis.js#property=667bf64dd99dc7001ae46dc9&product=sop";
-    script.async = true;
-    document.head.appendChild(script);
-  }
-};
--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=667bf64dd99dc7001ae46dc9&product=sop' async='async'></script>
-  ]);
-};
