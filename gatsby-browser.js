2d82c204cf863231a5d28bbac1cc17c2bf671c9a
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-06-26 15:56:00 +0200

    check all activated code (some of them does not work)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import "normalize.css";
 export const onClientEntry = () => {
   if (typeof window !== "undefined") {
     const script = document.createElement("script");
-    script.src = "https://platform-api.sharethis.com/js/sharethis.js#property=594d07a33b4e8e0012f0a4c8&product=inline-share-buttons";
+    script.src = "https://platform-api.sharethis.com/js/sharethis.js#property=667bf64dd99dc7001ae46dc9&product=sop";
     script.async = true;
     document.head.appendChild(script);
   }
