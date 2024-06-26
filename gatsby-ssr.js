c3bb525df86bad0161c3a4aacff1a5a5f23cba5e
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-06-26 14:47:30 +0200

    change script activation code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,10 +2,6 @@ import React from "react";
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <script 
-      type='text/javascript' 
-      src='https://platform-api.sharethis.com/js/sharethis.js#property=667bf64dd99dc7001ae46dc9&product=sop' 
-      async='async'
-    />,
+    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=594d07a33b4e8e0012f0a4c8&product=inline-share-buttons' async='async'></script>
   ]);
 };
