4aeeb1d1c35559c3d85de6a2bcf99543bb03b4a4
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-08-07 16:20:35 +0200

    add position title to json; rewrite experience structure

--- src/components/about.js
+++ src/components/about.js
@@ -27,29 +27,12 @@ const About = () => {
         <h1 className={`${AboutStyle.heading1} ${TypographyStyle.textWhite} ${AboutStyle.headingBold}`} id="about">{aboutInfo.name}</h1>
         <h2 className={`${TypographyStyle.heading2} ${TypographyStyle.textYellow} ${AboutStyle.textUppercase} ${AboutStyle.position}`}>{aboutInfo.position}</h2>
         <p className={`${AboutStyle.font18} ${TypographyStyle.textWhite} ${AboutStyle.description}`}>{aboutInfo.description}</p>
-        <a href="/Anastasiya_Kharitonova.pdf" download rel="noopener noreferrer" className={`${AboutStyle.font18} ${AboutStyle.buttonYellow} ${AboutStyle.buttonShineAnim}`}><span>Download a CV</span></a>
+        <a href="/CV-Frontend-developer.pdf" download rel="noopener noreferrer" className={`${AboutStyle.font18} ${AboutStyle.buttonYellow} ${AboutStyle.buttonShineAnim}`}><span>Download a CV</span></a>
       </div>
       <div className={AboutStyle.bioDecoration}>
         <p className={`${TypographyStyle.font80} ${AboutStyle.headingRegular} ${AboutStyle.textDarkGray}`}>CSS <br /> is <br /> Awesome</p>
       </div>
     </div>
-    {/* <div className={`${LayoutStyle.container}`}>
-      <ul className={`${TypographyStyle.textWhite} ${AboutStyle.aboutEducation}`}>
-        {aboutInfo.education.map((item, index) => (
-          <li key={index}>
-            {item.title} (<b>{item.school}, <i>{item.years}</i></b>)
-          </li>
-        ))}
-      </ul>
-
-      <ul className={`${TypographyStyle.textWhite} ${AboutStyle.aboutLanguages}`}>
-        {aboutInfo.languages.map((language, i) => (
-          <li key={i}>
-            {language.title} ({language.level})
-          </li>
-        ))}
-      </ul>
-    </div> */}
     </>
   )
 }
--- src/components/experience.js
+++ src/components/experience.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import * as ExperienceStyle from './experience.module.css'
 import * as LayoutStyle from './../styles/layout.module.css'
 import * as TypographyStyle from './../styles/typography.module.css'
@@ -21,15 +21,54 @@ const Experience = ({ blockTitle }) => {
       }
     }
   `)
-
   const experienceData = data.allFile.nodes[0].childDataJson.employment
+  const [isVisibleItems, setIsVisibleItems] = useState(new Array(experienceData.length).fill(false));
+  const refs = useRef(experienceData.map(() => React.createRef()))
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    }
+
+    const refsCurrent = refs.current;
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const index = refsCurrent.indexOf(entry.target)
+        if(entry.isIntersecting) {
+          setIsVisibleItems(prevVisibleItems => {
+            const newVisibleItems = [...prevVisibleItems];
+            newVisibleItems[index] = true;
+            return newVisibleItems;
+          });
+          observer.unobserve(entry.target)
+        }
+      });
+    }, options);
+
+    refsCurrent.forEach(ref => {
+      if(ref.current) {
+        observer.observe(ref.current);
+      }
+    });
+
+    return () => {
+      refsCurrent.forEach(ref => {
+        if(ref.current) {
+          observer.unobserve(ref.current);
+        }
+      });
+    }
+  }, [])
 
   return (
     <div className={`${LayoutStyle.vMargins30} ${LayoutStyle.marginBottom100}`} id="experience">
       <h2 className={`${TypographyStyle.heading2} ${TypographyStyle.textWhite} ${TypographyStyle.textCenter} ${LayoutStyle.yellowBottomLine}`}>{blockTitle}</h2>
-      <ul className={`${LayoutStyle.displayBlock} ${LayoutStyle.flex} ${LayoutStyle.fColumn} ${LayoutStyle.gap30}`}>
+      <section className={`${LayoutStyle.displayBlock} ${LayoutStyle.flex} ${LayoutStyle.fColumn} ${LayoutStyle.gap30}`}>
         {experienceData.map((item, index) => (
-          <li key={index} className={`${LayoutStyle.displayBlock} ${LayoutStyle.flex} ${LayoutStyle.fRow} ${ExperienceStyle.listStyleNone}`}>
+          <article ref={refs.current[index]} key={index} className={`${LayoutStyle.displayBlock} ${LayoutStyle.flex} ${LayoutStyle.fRow} ${ExperienceStyle.listStyleNone} ${ExperienceStyle.experienceArticle} ${isVisibleItems[index] ? 'visible' : 'hidden'}`}>
             <div className={`${LayoutStyle.hMargins15} ${TypographyStyle.headingRegular} ${ExperienceStyle.time}`}>
               {item.time}
             </div>
@@ -38,13 +77,13 @@ const Experience = ({ blockTitle }) => {
               <i> {item.company}</i>
               <ul className={`${LayoutStyle.vMargins30} ${ExperienceStyle.listBulletsInside}`}>
                 {item.description.map((i, key) => (
-                  <li key={key}>{i}</li>
+                  <li key={key} className={`${ExperienceStyle.experienceItem}`}>{i}</li>
                 ))}
               </ul>
             </div>
-          </li>
+          </article>
         ))}
-      </ul>
+      </section>
     </div>
   )
 }
